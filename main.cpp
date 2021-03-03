95fc605dbbd57259bd36c7f55538f24c2aefc7ce
T440 <conglinxiyi@gmail.com>
2021-03-03 12:28:50 -0800

    partially fixed the leak on main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,11 @@ std::string Print(Base* ptr)
         i.current_node()->accept(lv,i.current_index());
         i.next();
     }
+    std::string out = "";
     std::cout << lv->PrintLaTeX() << std::endl;
+    out = lv->PrintLaTeX();
+    delete lv;
+    return out;
     
 }
 
@@ -26,27 +30,24 @@ std::string PrintML(Base* ptr)
         i.current_node()->accept(mlv,i.current_index());
         i.next();
     }
-    std::cout<<mlv->ented()<<std::endl;
+    std::string out;
+    std::cout<<mlv->end()<<std::endl;
+    out = mlv->end();
+    delete mlv;
+    return out;
 }
 
 int main()
 {
-    Base* six = new Op(6);
-    Base* seven = new Op(7);
-    Base* four = new Op(4);
     Base* five = new Op(5);
-    Base* mult = new Mult(seven, four);
-    Base* add = new Add(six, mult);
-    Base* minus = new Sub(add, five);
-    Base* div = new Div(minus, five);
-    Base* pow = new Pow(div, five);
-
 	Base* one = new Op(1);
 	Base* zero = new Op(0);
 	Base* sub1 = new Sub(five,zero);
 	Base* add1 = new Add(one,sub1);
 
     std::cout << add1->stringify() << " = " << add1->evaluate() << std::endl;
-    Print(pow);
+    Print(add1);
 	PrintML(add1);
+
+    delete (five,one,zero,sub1,add1);
 }
